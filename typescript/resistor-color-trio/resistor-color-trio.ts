17b3bf16084f08602d3ede903401e7898a3b121a
Jesus <jesus.junc10@gmail.com>
2023-10-30 00:17:00 -0500

    documentation added

--- typescript/resistor-color-trio/resistor-color-trio.ts
+++ typescript/resistor-color-trio/resistor-color-trio.ts
@@ -3,11 +3,15 @@ export function decodedResistorValue(colors: string[]) {
   const secondValue = COLORS.indexOf(colors[1]);
   const numberOfZeros = COLORS.indexOf(colors[2]);
 
-  let resistorValue: string = `${fisrtValue !== 0 ? fisrtValue: ''}${secondValue}`;
+  // number whose first value if is zero is not considered
+  let resistorValue: string = `${fisrtValue !== 0 ? fisrtValue : ''}${secondValue}`;
+  // declaration for the unity of the resistor value
   let unity:string = '';
 
+  // number of zeros to be added to the resistor value
   resistorValue = resistorValue.concat("0".repeat(numberOfZeros));
 
+  // add the unity to the resistor value
   [unity, resistorValue] = unityConversor(+resistorValue);
 
   return `${resistorValue} ${unity}ohms`;
@@ -35,12 +39,12 @@ function unityConversor (value: number) {
     unity = "kilo"
     value = value / 1000;
   }
-  if (notZero && value % 1000 == 0)
+  if (notZero && value % 1000 === 0)
   {
     unity = "mega"
     value = value / 1000;
   }
-  if (notZero && value % 1000 == 0)
+  if (notZero && value % 1000 === 0)
   {
     unity = "giga"
     value = value / 1000;
