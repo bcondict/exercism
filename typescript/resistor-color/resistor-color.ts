7ddcdb2b870dab6bc76fab2cbff705224d8262b6
Jesus <jesus.junc10@gmail.com>
2023-10-29 21:57:40 -0500

    resistant color task

---
+++ typescript/resistor-color/resistor-color.ts
@@ -0,0 +1,17 @@
+export const colorCode = (color:string): number  => {
+  // eslint-disable-next-line @typescript-eslint/no-use-before-define
+  return COLORS.indexOf(color)
+}
+
+export const COLORS = [
+  'black',
+  'brown',
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'blue',
+  'violet',
+  'grey',
+  'white'
+]
